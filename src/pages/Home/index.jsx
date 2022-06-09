445467f2e536285a9dc1f8c1bdc575b3f04b4f5a
moiid212834 <moeed27@gmail.com>
2022-06-09 22:53:23 +0500

    bug fix to allow deploy

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Search from '../../components/search';
 import Suggestions from '../../components/artistSuggestions';
-import SearchResults from '../../components/search/SearchResults';
+import SearchResults from '../../components/searchResults';
 import {Container} from '@mui/system';
 
 export default function Home() {
